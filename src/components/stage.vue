70e7f1a99fefde68c9b9e7c80650e3f7258b027e
pottiri2405 <kaneyasu2405@gmail.com>
2020-10-04 20:21:46 +0900

    no message

--- src/components/stage.vue
+++ src/components/stage.vue
@@ -1,21 +1,10 @@
 <template>
   <div id="stage">
-    <b-progress :max="progressMax" height="2rem">
-      <b-progress-bar :value="progress">
-        <span>Progress: <strong>{{ progress.toFixed(2) }} / {{ progressMax }}</strong></span>
-      </b-progress-bar>
-    </b-progress>
-    <!-- <b-alert
-      :show="dismissCountDown"
-      dismissible
-      variant="success"
-      @dismissed="dismissCountDown=0"
-      @dismiss-count-down="countDownChanged"
-      class="mt-3"
-    >
-      <p>Answer</p>
-    </b-alert> -->
-    <b-container class="mt-3">
+    <b-container>
+      <b-row>
+        <b-col class="text-center">
+        </b-col>
+      </b-row>
       <b-row v-for="(cols, y) in rows" :key="'row-' + y">
         <b-link href="#foo" v-for="(col, x) in cols" :key="'col-' + y + '-' + x" @click="choice(x, y)">
           <b-col border class="box" v-bind:class="{'box-row-last': isLast(y, rows.length - 1), 'box-col-last': isLast(x, cols.length - 1)}" bg-variant="info">
@@ -24,21 +13,31 @@
         </b-link>
       </b-row>
     </b-container>
-    <b-button size="md" variant="danger" v-b-toggle:stage-words class="mt-3">
-      <span class="when-open">
-          <small><b-icon icon="arrows-collapse" />Close Answer</small>
-      </span>
-      <span class="when-closed">
-          <small><b-icon icon="arrows-expand" />Open Answer</small>
-      </span>
-    </b-button>
-    <b-collapse id="stage-words" class="mt-3">
-      <ul id="word-list">
-        <li v-for="(word, i) in words" :key="'word-list' + i">
-          {{ word.text }}
-        </li>
-      </ul>
-    </b-collapse>
+    <b-progress :max="progressMax" height="2rem" class="mt-3 ml-3 w-75">
+      <b-progress-bar :value="progress">
+        <span>Progress: <strong>{{ progress.toFixed(2) }} / {{ progressMax }}</strong></span>
+      </b-progress-bar>
+    </b-progress>    
+    <p class="text-left mt-3 ml-3">
+      <b-button size="sm" variant="danger">
+        <b-icon icon="question-square" class="mr-2" />Open hint
+      </b-button>
+      <b-button size="sm" v-b-modal.modal-nice variant="secondary">
+        <b-icon icon="question-square" class="mr-2" />Modal
+      </b-button>      
+    </p>
+    <div id="hint-area" class="mt-3 ml-3  w-75">
+      <b-badge pill variant="secondary" v-for="(hint, i) in hints" :key="'hint-list-' + i" class="mr-1">
+        <font size="+0">
+          <b-icon icon="question" v-for="n of hint.text.length" :key="n">
+          </b-icon>
+        </font>
+      </b-badge>
+    </div>
+    <b-modal id="modal-nice" title="Nice!" header-bg-variant="success" ok-only>
+      <p><font size="+1">Hello from modal!</font></p>
+      <p><font size="+0">Hello from modal!</font></p>
+    </b-modal>    
   </div>
 </template>
 
@@ -63,19 +62,41 @@ export default {
         [1, 2, 3, 4, 5, 6, 7, 8, 9, '１'],
         [1, 2, 3, 4, 5, 6, 7, 8, 9, '１']
       ],
-      words: [
-        {text: 123, comment: 12345},
-        {text: 456, comment: 12345},
-        {text: 789, comment: 12345},
-        {text: 123, comment: 12345},
-        {text: 456, comment: 12345},
-        {text: 789, comment: 12345},
-        {text: 123, comment: 12345},
-        {text: 456, comment: 12345},
-        {text: 789, comment: 12345},
-        {text: 123, comment: 12345},
-        {text: 456, comment: 12345},
-        {text: 789, comment: 12345}                        
+      hints: [
+        {text: '12３', comment: 12345},
+        {text: '１２３', comment: 12345},
+        {text: '789', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: '7891011', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: 'あいうえお', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: '12３', comment: 12345},
+        {text: '１２３', comment: 12345},
+        {text: '789', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: '7891011', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: 'あいうえお', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: '12３', comment: 12345},
+        {text: '１２３', comment: 12345},
+        {text: '789', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: '7891011', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},
+        {text: 'あいうえお', comment: 12345},
+        {text: '123', comment: 12345},
+        {text: '456', comment: 12345},                
+        {text: '789', comment: 12345}                        
       ]
     }
   },
