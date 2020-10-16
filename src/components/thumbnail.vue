7fde5b2c41193c33c8d20358c308ab608e98262f
pottiri2405 <kaneyasu2405@gmail.com>
2020-10-16 20:07:26 +0900

    no message

--- src/components/thumbnail.vue
+++ src/components/thumbnail.vue
@@ -1,52 +1,67 @@
 <template>
   <div>
-    <b-form-input v-model="image"></b-form-input>    
-    <b-form-input v-model="color"></b-form-input>    
-    <b-form-input v-model="position"></b-form-input>    
-    <b-card
-      id="preview"
-      no-body
-      header="header"
-      header-bg-variant="green"
-      header-border-variant="dark"
-      footer="footer"
-      footer-bg-variant="light"
-      footer-border-variant="dark"
-    >
-      <template v-slot:header>
-        <h5 class="mb-0 text-center align-middle">{{ $t("message.title") }}</h5>
-      </template>
-      <b-card-body class="text-center align-middle" :style="{ backgroundImage: 'url(' + image + ')', 'background-size': 'cover', 'background-position': position  }">
-        <b-container :style="{ color: color }">
-          <b-row>
-            <b-col class="text-center align-middle">
-              <h3>{{ data.title }}</h3>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col class="text-center align-middle">
-              <h5>{{ $t("message.thumbnail_size") }}{{ data.size }} ✕ {{ data.size }}</h5>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col class="text-center align-middle">
-              <h5>{{ $t("message.thumbnail_word_count") }}{{ Object.keys(data.words).length }}</h5>
-            </b-col>
-          </b-row>
-        </b-container>
-      </b-card-body>
-      <template v-slot:footer>
-        <p class="text-center align-middle">
-          <b-badge pill variant="primary" v-for="(tag, i) in data.tags" :key="'tag-' + i" class="tag mr-1">
-            {{ tag }}
-          </b-badge>
-        </p>
-      </template>
-    </b-card>
-    <b-button size="md" variant="primary" class="mt-3 mb-3" @click="generate()">
-      画像作成
-    </b-button>
-    <div id="result"></div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <b-form-select v-model="image"
+          :options="[
+          {value: '/static/background/fruit.jpg', text: 'フルーツ'},
+          {value: '/static/background/moon.jpg', text: '月'},
+          {value: '/static/background/wisteria.jpg', text: '藤の花'},
+          {value: '/static/background/monstar-ball,jpg', text: 'モンスターボール'},
+          ]"
+          size="sm"
+          class="mt-3">
+          </b-form-select>
+          <b-form-input v-model="image"></b-form-input>    
+          <b-form-input v-model="color" size="sm" ></b-form-input>    
+          <b-form-input v-model="position" size="sm" ></b-form-input>    
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <p class="mt-3 mb-3"></p>
+          <b-card
+            id="preview"
+            no-header
+            no-body
+            footer-bg-variant="light"
+            footer-border-variant="dark"
+          >
+            <b-card-body class="text-center align-middle" :style="{ backgroundImage: 'url(' + image + ')', 'background-size': 'cover', 'background-position': position  }">
+              <b-container :style="{ color: color }">
+                <b-row>
+                  <b-col class="text-center align-middle">
+                    <h3><b>{{ data.title }}</b></h3>
+                    <h5><b>{{ $t("message.title") }}</b></h5>
+                  </b-col>
+                </b-row>
+              </b-container>
+            </b-card-body>
+            <template v-slot:footer>
+              <p class="text-center align-middle">
+                <b-badge variant="primary" class="tag mr-1">
+                  {{ $t('message.thumbnail_size', { size: data.size }) }}
+                </b-badge>
+                <b-badge variant="primary" class="tag">
+                  {{ $t('message.thumbnail_word_count', { count: Object.keys(data.words).length }) }}
+                </b-badge>
+              </p>
+              <p class="text-center align-middle">
+                <b-badge variant="info" v-for="(tag, i) in data.tags" :key="'tag-' + i" class="tag mr-1">
+                  {{ tag }}
+                </b-badge>
+              </p>
+            </template>
+          </b-card>
+          <p class="mt-3 mb-3"></p>
+          <b-button size="md" variant="primary" class="mt-3 mb-3" @click="generate()">
+            画像作成
+          </b-button>
+          <div id="result"></div>
+        </b-col>
+      </b-row> 
+    </b-container>           
   </div>
 </template>
 <script>
@@ -54,8 +69,8 @@ export default {
   data () {
     return {
       data: {title: '', size: 0, words: {}, tags: {}},
-      color: '#000000',
-      image: '/static/background/monstar-ball.jpg',
+      color: '#FFFFFF',
+      image: '/static/background/fruit.jpg',
       position: 'center center'
     }
   },
@@ -94,6 +109,7 @@ export default {
 }
 #preview .card {
   height: 100%;
+  border: 1px solid;
 }
 .bg-green {
   background-color: #009688 !important;
